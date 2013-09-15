aac575d73e22b686fa840b08a484973b8ecc3c29
Zack Anderson <zack@hush.es>
2013-09-15 14:55:55 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GardenMate::Application.config.secret_key_base = '72d156252d07547b6b9ddfe9e0b318f5ddb449ed4ad053aa78fa30bb1d2b7d387648f99ff7f647f831036b1e1530f8b3a82c7eef40c21c2fd7f8b9e7557653b8'
